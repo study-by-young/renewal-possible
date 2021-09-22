911b0507129889d045bd44b37efb356ca46642ad
suneogkwon <tnsdjr7@gmail.com>
2021-09-22 23:17:37 +0900

    edit xmls

--- Possible/src/main/java/com/yedam/possable/app/HomeController.java
+++ Possible/src/main/java/com/yedam/possable/app/HomeController.java
@@ -1,4 +1,4 @@
-package com.yedam.pos;
+package com.yedam.possable.app;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -17,26 +17,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "home";
 	}
-	
+
 	@GetMapping("/test")
 	public String test() {
 		return "test/test";
