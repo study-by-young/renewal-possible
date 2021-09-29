a62b5a46ed39db35a13e1276efcfb2e308e5fc83
mxmrun13@naver.com <mxmrun13@naver.com>
2021-09-29 18:35:19 +0900

    tour java

--- Possible/src/main/java/com/yedam/possable/app/community/tour/domain/TestVO.java
+++ Possible/src/main/java/com/yedam/possable/app/community/tour/domain/TestVO.java
@@ -1,23 +1,27 @@
 package com.yedam.possable.app.community.tour.domain;
 
+import lombok.Data;
+
+@Data
 public class TestVO {
 	
-	private String addr;
+	private String addr1;
+	private int areaCode;
+	private String cat1;
+	private String cat2;
+	private String cat3;
+	private int contentId;
+	private int contentTypeId;
+	private long createdTime;
+	private String firstImage;
+	private String firstImage2;
+	private Double mapX;
+	private Double mapY;
+	private int mLevel;
+	private long modifiedTime;
+	private int readCount;
+	private int sigunguCode;
 	private String title;
+	private String zipCode;
 	
-	public TestVO() {}
-	
-	public String getAddr() {
-		return addr;
-	}
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 }
---
+++ Possible/src/main/java/com/yedam/possable/app/community/tour/mapper/TourMapper.java
@@ -0,0 +1,9 @@
+package com.yedam.possable.app.community.tour.mapper;
+
+import com.yedam.possable.app.community.tour.domain.TestVO;
+
+public interface TourMapper {
+
+	public int insert(TestVO vo);
+	
+}
