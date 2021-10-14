14e80764a704d8a00480fb3945f4e549f98e5ad0
kwonsuneog <tnsdjr7@gmail.com>
2021-10-14 11:02:37 +0900

    edit notice controller

--- Possible/src/main/java/com/yedam/possable/app/community/notice/controller/NoticeController.java
+++ Possible/src/main/java/com/yedam/possable/app/community/notice/controller/NoticeController.java
@@ -14,65 +14,61 @@ import com.yedam.possable.app.community.notice.service.NoticeService;
 @Controller
 @RequestMapping("/notice/*")
 public class NoticeController {
-	
+
 	@Autowired
 	NoticeService noticeService;
-	
+
 	@GetMapping("/list")
 	public void list(Model model) {
 		model.addAttribute("list", noticeService.getList());
-		model.addAttribute("title", "공지사항 리스트");
 	}
-	
+
 	@GetMapping("/get")
 	public void get(Model model, NoticeVO vo) {
 		model.addAttribute("notice", noticeService.read(vo));
-		model.addAttribute("title", "단건 조회");
 	}
-	
+
 	@GetMapping("/insert")
 	public void insertForm(Model model) {
-		model.addAttribute("title", "등록폼");
+
 	}
-	
+
 	@PostMapping("/insert")
 	public String insert(Model model, RedirectAttributes rttr, NoticeVO vo) {
-		model.addAttribute("title", "등록");
 		noticeService.insert(vo);
 		rttr.addFlashAttribute("insertResult", vo.getSeq());
-		
+
 		return "redirect:/notice/list";
 	}
-	
+
 	@GetMapping("/update")
 	public void updateForm(Model model, NoticeVO vo) {
-		model.addAttribute("title", "수정폼");
 		model.addAttribute("notice", noticeService.read(vo));
 	}
-	
+
 	@PostMapping("/update")
 	public String update(RedirectAttributes rttr, NoticeVO vo) {
-		
+
 		int result = noticeService.update(vo);
-		
+
 		if (result == 1) {
 			rttr.addFlashAttribute("updateResult", vo.getSeq());
 		}
-		
+
 		rttr.addAttribute("seq", vo.getSeq());
-		
+
 		return "redirect:/notice/get";
 	}
 
 	@GetMapping("/delete")
 	public String delete(RedirectAttributes rttr, NoticeVO vo) {
-		
+
 		int result = noticeService.delete(vo);
-		
+
 		if (result == 1) {
 			rttr.addFlashAttribute("deleteResult", vo.getSeq());
 		}
-		
+
 		return "redirect:/notice/list";
 	}
 }
